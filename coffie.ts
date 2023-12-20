2ed098a409de8461fd26b40c6654b203cae0dbe6
nhannguyen789 <152852440+nhantrong789@users.noreply.github.com>
2023-12-20 14:38:21 +0700

    Add files via upload

---
+++ coffie.ts
@@ -0,0 +1,16 @@
+<!-- ==== HEADERWRAP ==== -->
+<div id="home-section">
+<div id="headerwrap" name="home">
+  <header class="clearfix tb"> 
+ <div class="tb-cell text-center">
+ <i class="fa fa-heart-o icon"></i>
+    <h1>Coffee - Makes you Love</h1>
+    <p>A Lot Can Happen Over Coffee, Its Awesome!!! </p>
+    <a href="#portfolio" class="smoothScroll btn btn-lg">Today's Special</a> 
+	</div>
+	</header>
+</div>
+
+</div>
+<div class="div-pattern2"></div>
+<!-- /headerwrap --> 
